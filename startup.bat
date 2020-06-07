3d4708d9960b1c8505b279804db271d83c002d2d
明月 <1093286982@qq.com>
2020-06-07 23:42:55 +0800

    build nanohttpd for simple web server
    Signed-off-by: 明月 <1093286982@qq.com>

--- startup.bat
+++
@@ -1,2 +0,0 @@
-ipconfig | grep -i "ipv4" | grep -Po "(?<=: )[\d.]+"
-python -m http.server 3000
---
+++ startup.sh
@@ -0,0 +1,3 @@
+ipconfig | grep -i "ipv4" | grep -Po "(?<=: )[\d.]+"
+java -jar httpserver.jar -p 3000
+# python -m http.server 3000
